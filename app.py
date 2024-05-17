6f2b35a9fe10a1a00b8bb3c866acda522f01b4e5
Mahmoud nagi <167598000+mahmoudnagi22@users.noreply.github.com>
2024-05-18 01:05:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,36 @@
-from flask import Flask, render_template, request, jsonify
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
+def initialize_model():
+    path = "jianghc/medical_chatbot"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tokenizer = GPT2Tokenizer.from_pretrained(path)
+    model = GPT2LMHeadModel.from_pretrained(path).to(device)
+    return tokenizer, model, device
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+def generate_response(input_text, tokenizer, model, device):
+    prompt_input = (
+        "The conversation between human and AI assistant.\n"
+        "[|Human|] {input}\n"
+        "[|AI|]"
+    )
+    sentence = prompt_input.format(input=input_text)
+    inputs = tokenizer(sentence, return_tensors="pt").to(device)
 
+    with torch.no_grad():
+        beam_output = model.generate(
+            **inputs,
+            min_new_tokens=1, 
+            max_length=512,
+            num_beams=3,
+            repetition_penalty=1.2,
+            early_stopping=True,
+            eos_token_id=198
+        )
+    return tokenizer.decode(beam_output[0], skip_special_tokens=True)
 
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template('chat.html')
-
-
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    input = msg
-    return get_Chat_response(input)
-
-
-def get_Chat_response(text):
-
-# Let's chat for 5 lines
-    for step in range(5):
-    # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
-
-    # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-    # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-    # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    tokenizer, model, device = initialize_model()
+    user_input = "what is Parkinson's disease?"
+    response = generate_response(user_input, tokenizer, model, device)
+    print(response)
